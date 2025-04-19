ed8b21eb19cb2dbe3343fc063a552c766f4393a3
Nancy Malgapo <nansimalgapo@gmail.com>
2025-04-19 11:48:51 +0200

    Added new schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,23 @@
-from typing import Any
-
+from datetime import date
+from typing import Any, Dict
 from pydantic import BaseModel
 
 
 class Output(BaseModel):
     message: str | None = None
     results: Any | None = None
+
+
+class ConvertionRatesModel(BaseModel):
+    date: date
+    conversion_rates: Dict
+
+
+class ExchangeRateByDateModel(BaseModel):
+    date: date
+    currency: str
+    exchange_rate: float
+
+
+class HistoricalDataModel(BaseModel):
+    result: Dict
