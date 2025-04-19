664e7caaa6df0b94a3c47ea18c34179aa06f2bc2
Nancy Malgapo <nansimalgapo@gmail.com>
2025-04-19 21:07:45 +0200

    Added new test

--- tests/utils/test_helper.py
+++ tests/utils/test_helper.py
@@ -1,19 +1,46 @@
 import pytest
 import pandas as pd
 
-from datetime import datetime
-from utils.helper import format_date, clean_data
+from datetime import datetime, timedelta
+from utils.helper import format_date, validate_currency, validate_date, clean_data
 
 def test_format_date():
     date_str = "2023-01-01"
     expected_date = datetime(2023, 1, 1)
     assert format_date(date_str) == expected_date
 
-
-def test_format_date_invalid():
     date_str = "01-01-2023"
     with pytest.raises(ValueError):
         format_date(date_str)
+
+
+def test_validate_currency():
+    # Test with a valid currency
+    assert validate_currency("USD") == (True, "Success")
+
+    # Test with EUR currency
+    assert validate_currency("EUR") == (False, "Currency cannot be EUR")
+
+    # Test with an invalid currency length
+    assert validate_currency("$USD") == (False, "Currency code must be exactly 3 characters long")
+
+
+def test_validate_date():
+    # Test with today's date
+    today_date_str = datetime.now().strftime('%Y-%m-%d')  # Today's date
+    assert validate_date(today_date_str) == (True, "Success")
+
+    # Test with a date not compliant to YYYY-MM-DD format
+    date_str = "01/01/2023"
+    assert validate_date(date_str) == (False, "Invalid date format")
+
+    # Test with a future date
+    future_date_str = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')  # Tomorrow's date
+    assert validate_date(future_date_str) == (False, "Date cannot be in the future")
+
+    # Test with an empty string
+    date_str = ""
+    assert validate_date(date_str) == (False, "Invalid date format")
 
 
 def test_clean_data():
--- utils/helper.py
+++ utils/helper.py
@@ -4,10 +4,52 @@ import requests
 import zipfile
 
 from datetime import datetime
+from typing import Tuple
 
 
-def format_date(str_date: str):
+def format_date(str_date: str) -> datetime:
     return datetime.strptime(str_date, '%Y-%m-%d')
+
+
+def validate_currency(currency: str) -> Tuple[bool, str]:
+    """
+    Validates the input currency string.
+
+    1. Checks if the length of the currency string is exactly 3.
+    2. Checks if the currency is not equal to 'EUR'.
+
+    Returns:
+        Tuple[bool, str]: A tuple containing a boolean indicating validity and a message.
+    """
+    if len(currency) != 3:
+        return False, "Currency code must be exactly 3 characters long"
+
+    if currency == "EUR":
+        return False, "Currency cannot be EUR"
+
+    return True, "Success"
+
+
+def validate_date(str_date: str) -> Tuple[bool, str]:
+    """
+    Validates the input date string.
+
+    1. Checks if the date is in the future.
+    2. Checks if the date format is YYYY-MM-DD.
+
+    Returns:
+        Tuple[bool, str]: A tuple containing a boolean indicating validity and a message.
+    """
+    try:
+        input_date = format_date(str_date)
+    except ValueError:
+        return False, "Invalid date format"
+
+    today = datetime.now()
+    if input_date > today:
+        return False, "Date cannot be in the future"
+
+    return True, "Success"
 
 
 def clean_data(df: pd.DataFrame):
