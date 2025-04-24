71c5f57520f2ed1a5e3c273ec6dda7a743752d11
Nancy Malgapo <nansimalgapo@gmail.com>
2025-04-24 15:32:09 +0200

    Improved exception class

--- app/errors.py
+++ app/errors.py
@@ -5,32 +5,28 @@ from fastapi import HTTPException, status
 class ErrorTypes(str, Enum):
     UNKNOWN = "unknown"
     BAD_REQUEST = "bad_request"
-    NOT_FOUND = "not_found"
-    INTERNAL_SERVER_ERROR = "internal_server_error"
+    NOT_FOUND = "not found"
+    INTERNAL_SERVER_ERROR = "internal server error"
 
 
-class BaseAppException(HTTPException):
+class ApiException(HTTPException):
     http_status_code: int
     error_type: ErrorTypes
-    details: str
 
-    def __init__(self):
-        super().__init__(status_code=self.http_status_code, detail=self.details)
+    def __init__(self, detail: str):
+        super().__init__(status_code=self.http_status_code, detail=detail)
 
 
-class BadRequestException(BaseAppException):
+class BadRequestException(ApiException):
     http_status_code = status.HTTP_400_BAD_REQUEST
     error_type = ErrorTypes.BAD_REQUEST
-    details = "Bad request"
 
 
-class NotFoundException(BaseAppException):
+class NotFoundException(ApiException):
     http_status_code = status.HTTP_404_NOT_FOUND
     error_type = ErrorTypes.NOT_FOUND
-    details = "Not Found"
 
 
-class InternalServerErrorException(BaseAppException):
+class InternalServerErrorException(ApiException):
     http_status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     error_type = ErrorTypes.INTERNAL_SERVER_ERROR
-    details = "Internal Server Error"
